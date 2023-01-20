4f1fa1d14bd30db8d6bb921bd09259d478241a7c
Christopher Wellons <wellons@nullprogram.com>
2023-01-19 22:08:21 -0500

    Correct SYS_close call number

--- linux_amd64_main.c
+++ linux_amd64_main.c
@@ -10,9 +10,9 @@
      "/usr/share/pkgconfig"
 #endif
 
-#define SYS_close 0
 #define SYS_write 1
 #define SYS_open  2
+#define SYS_close 3
 #define SYS_fstat 5
 #define SYS_mmap  9
 #define SYS_exit  60
